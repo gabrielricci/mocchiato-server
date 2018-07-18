43eb669d56939619bef8ee01828b72be98c25554
Gabriel Ricci <gabriel.ricci@sumup.com>
2018-07-18 20:15:40 -0300

    First version

---
+++ app.py
@@ -0,0 +1,17 @@
+#!flask/bin/python
+from flask import Flask, request
+import time
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST', 'PUT', 'PATCH'])
+@app.route('/<path:full_path>', methods=["GET", "POST", "PUT", "PATCH"])
+def index(full_path=None):
+    sleep_time = int(request.args.get('sleep', 0))
+    if sleep_time > 0:
+        time.sleep(sleep_time)
+
+    return request.get_data()
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5555, debug=True)
